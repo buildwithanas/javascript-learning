cacfbb9d534f90a28abaef9edb79033fc0e4c42c
buildwithanas <anasamoo0704@gmail.com>
2026-08-24 23:45:47 +0100

    Add Arrays & Loops Projects

--- 11-Arrays_&_Loops/script/todo-list.js
+++ 11-Arrays_&_Loops/script/todo-list.js
@@ -1,4 +1,10 @@
-const todoList = [];
+const todoList = ['writecode', 'make dinner'];
+
+for(let i = 0; i < todoList.length; i++) {
+  const todo = todoList[i];
+  const html = `<p>${todo}</p>`
+}
+
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
   const name = inputElement.value;
