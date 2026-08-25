f4a5273ea3d57d066a861a7da893047a46d91830
buildwithanas <anasamoo0704@gmail.com>
2026-08-26 00:11:20 +0100

    Add Array and loops practice

--- 11-Arrays_&_Loops/script/todo-list.js
+++ 11-Arrays_&_Loops/script/todo-list.js
@@ -11,10 +11,13 @@ function renderTodoList() {
   let todoListHTML = '';
 
   for (let i = 0; i < todoList.length; i++) {
-    const todo = todoList[i];
+    const todoObject = todoList[i];
+    //const name = todoObject.name;
+    //const dueDate = todoObject.dueDate;
+    const { name, dueDate } = todoObject
     const html = `
     <p>
-    ${todo} 
+    ${name} ${dueDate}
     <button onclick="
       todoList.splice(${i}, 1);
       renderTodoList();
@@ -24,7 +27,6 @@ function renderTodoList() {
     todoListHTML += html;
   }
 
-  console.log(todoListHTML);
 
   document.querySelector('.js-todo-list')
   .innerHTML = todoListHTML;
@@ -34,8 +36,15 @@ function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
   const name = inputElement.value;
 
-  todoList.push(name);
-  console.log(todoList);
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+
+  todoList.push({
+   // name: name,
+   // dueDate: dueDate,
+    name,
+    dueDate 
+});
 
   inputElement.value = '';
   renderTodoList();
