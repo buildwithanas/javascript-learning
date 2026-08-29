a10968d035a7ec28300bddd86fda8690480d925b
buildwithanas <anasamoo0704@gmail.com>
2026-08-29 22:53:40 +0100

    Add Advanced Function Practice

--- 05-Functions/Advanced_function/rps.js
+++ 05-Functions/Advanced_function/rps.js
@@ -62,7 +62,13 @@ document.querySelector('.js-scissors-btn')
 
 
 document.body.addEventListener('event', () => {
-  if ('event.key === 'r');
+  if (event.key === 'r') {
+    playGame('rock');
+  } else if (event.key === 'p') {
+    playGame('paper');
+  }else if (event.key === 's') {
+    playGame('scissors');
+  }
 });
 
     function playGame(playerMove) {
